5ad80c9f32f417ab493dab33473d76f82b27b3cc
yooshima <o.shimayu891@gmail.com>
2024-04-27 10:15:07 +0900

    04271st

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: yooshima <yooshima@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/24 15:23:57 by yooshima          #+#    #+#              #
-#    Updated: 2024/04/26 18:21:36 by yooshima         ###   ########.fr        #
+#    Updated: 2024/04/26 18:39:17 by yooshima         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,7 +18,7 @@ SRCS	= ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c\
 			ft_memchr.c ft_memcmp.c ft_strlcpy.c ft_strnstr.c ft_strlcat.c\
 			ft_atoi.c ft_calloc.c ft_strdup.c ft_substr.c ft_strjoin.c\
 			ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c\
-			ft_putchar_fd.c ft_putstr_fd ft_putendl_fd.c ft_putnbr_fd.c
+			ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
 OBJS	= $(SRCS:.c=.o)
 CC		= CC
 CFLAGS	= -Wall -Werror -Wextra
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: yooshima <yooshima@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 18:41:09 by yooshima          #+#    #+#             */
-/*   Updated: 2024/04/24 17:06:16 by yooshima         ###   ########.fr       */
+/*   Updated: 2024/04/26 19:13:48 by yooshima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,42 @@
 #include <stdio.h>
 #include "libft.h"
 
-//エラー処理が必要？？strlenがどちらかの文字列の長さ(\0含む)を超えた場合
-//https://hikotech.net/post-100/
-//check it
-
 void	*ft_memmove(void *dest, const void *src, size_t len)
 {
 	unsigned long	i;
 	char			*str1;
-	const char		*str2;
+	char			*str2;
 
 	str1 = dest;
-	str2 = src;
+	str2 = (char *)src;
+	if (dest > src)
+	{
+		len -= 1;
+		while(len >= 0)
+		{
+		str1[len] = str2[len];
+		len--;
+		}
+	}
+	else
+	{
 	i = 0;
 	while (i < len)
 	{
 		str1[i] = str2[i];
 		i++;
 	}
+	}
 	return (dest);
 }
 
-// int main(void)
-// {
-// 	int n = 3;
-// 	char or_dest[] = "abcdef";
-// 	// char or_src[] = "bbbbbbbb";
-// 	printf("or_dest = %s\n", memmove(or_dest + 2, or_dest, n));
-// 	char ft_dest[] = "abcdef";
-// 	// char ft_src[] = "bbbbbbbb";
-// 	printf("ft_dest = %s\n", ft_memmove(ft_dest + 2, ft_dest, n));
-// }
+int main(void)
+{
+	int n = 4;
+	char or_dest[] = "abcdef";
+	// char or_src[] = "bbbbbbbb";
+	printf("or_dest = %s\n", memmove(or_dest + 1, or_dest, n));
+	char ft_dest[] = "abcdef";
+	// char ft_src[] = "bbbbbbbb";
+	printf("ft_dest = %s\n", ft_memmove(ft_dest + 1, ft_dest, n));
+}
